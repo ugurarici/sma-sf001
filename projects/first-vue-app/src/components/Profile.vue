e86593dd25bb54e7018f7a485db7fa6493e26e1c
Uğur Arıcı <m.ugurarici@gmail.com>
2022-02-05 11:41:16 +0300

    Create our own project components in first-vue-app

---
+++ projects/first-vue-app/src/components/Profile.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Here is the profile of {{ name }}
+    <like-button />
+  </div>
+</template>
+
+<script>
+import LikeButton from "./partials/LikeButton.vue";
+
+export default {
+  props: ["name"],
+  components: {
+    LikeButton,
+  },
+};
+</script>
